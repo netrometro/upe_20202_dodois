86b6e85db1cf5ef2cc2d9e45a563bb7f5d10ade3
Savio Santos <saviosa08@gmail.com>
2021-07-23 19:22:12 -0300

    Testado projeto no VS Code. Funcionando corretamente.

--- src/main/java/br/upe/pweb/dodois/usuarios/model/Usuario.java
+++ src/main/java/br/upe/pweb/dodois/usuarios/model/Usuario.java
@@ -1,28 +1,20 @@
 package br.upe.pweb.dodois.usuarios.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import br.upe.pweb.dodois.base.Entidade;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import java.util.Date;
 
 @Data
 @Entity
 @EqualsAndHashCode
-@ToString
 public class Usuario extends Entidade {
 	private String nome;
-	private Date dataNasc;
 	
 	@Enumerated(EnumType.STRING)
 	private Sexo sexo;
