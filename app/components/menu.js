dbf229f09f21f607bb4e6bccb65d67a8887a6a7d
brenolyES <brenoly.porto@upe.br>
2021-07-30 17:57:04 -0300

    :sparkles: criado pags de recuperar acesso e de login

--- app/components/menu.js
+++ app/components/menu.js
@@ -5,11 +5,28 @@ export default class Menu extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-light bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">
-                            Tracker de Dodóis
-                        </a>
+                <nav class="navbar navbar-expand-md navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Tracker de Dodois</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Página Inicial</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Termos de Uso</a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Sobre</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contatos</a>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
