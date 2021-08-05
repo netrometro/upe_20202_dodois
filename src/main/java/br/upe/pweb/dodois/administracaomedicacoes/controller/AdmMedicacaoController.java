dad90637dcdee1b5f05295446858256ea66f2013
brenolyES <brenoly.porto@upe.br>
2021-08-05 15:35:33 -0300

    :sparkles: terminado pacote, entidade e crud de medicacoes

--- src/main/java/br/upe/pweb/dodois/administracaomedicacoes/controller/AdmMedicacaoController.java
+++ src/main/java/br/upe/pweb/dodois/administracaomedicacoes/controller/AdmMedicacaoController.java
@@ -1,7 +1,12 @@
 package br.upe.pweb.dodois.administracaomedicacoes.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,21 @@ public class AdmMedicacaoController {
     @PostMapping("/admmedicacao")
 	public AdmMedicacao criarEvento(@RequestBody AdmMedicacao admMedicacao) {
 		return this.servico.incluir(admMedicacao);
+	}
+
+	@GetMapping("/admmedicacoes")
+	public List<AdmMedicacao> listarMedicacoes(){
+		return (List<AdmMedicacao>) servico.listar();
+	}
+
+	@GetMapping("/admmedicacao/{id}")
+	public AdmMedicacao listarMedicacao(@PathVariable Long id){
+		return servico.procurar(id);
+	}
+
+	@DeleteMapping("/admmedicacao/{id}")
+	public void deletarMedicacao(@PathVariable Long id){
+		this.servico.excluir(id);
 	}
 
 }
