6fd8b1f57e5ccef7bfbcd34adc6c3807f900bf58
Helaine <helaine.lins@upe.br>
2021-08-12 10:53:19 -0300

    Ajustes credenciamento
    Alterações na implementação do Credenciamento e criação de usuário para automaticamente criar e associar grupos.

--- src/main/java/br/upe/pweb/dodois/usuario/servico/CredenciadoServico.java
+++ src/main/java/br/upe/pweb/dodois/usuario/servico/CredenciadoServico.java
@@ -1,36 +1,51 @@
 package br.upe.pweb.dodois.usuario.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import br.upe.pweb.dodois.usuario.dao.ICredenciadoDao;
 import br.upe.pweb.dodois.usuario.model.Credenciado;
 import br.upe.pweb.dodois.usuario.servico.interfaces.ICredenciadoServico;
 
 @Service
 public class CredenciadoServico implements ICredenciadoServico {
-	@Autowired private ICredenciadoDao dao;
+  @Autowired
+  private ICredenciadoDao dao;
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public ICredenciadoDao getDao() {
-		return this.dao;
-	}
+  @SuppressWarnings("unchecked")
+  @Override
+  public ICredenciadoDao getDao() {
+    return this.dao;
+  }
 
-	public boolean emailExiste(String email){
-		return getDao().existsByEmail(email);
-	}
+  @Override
+  public void validarIncluir(Credenciado credenciado) {
 
-	public boolean existe(Long id){
-		return getDao().existsById(id);
-	}
+    if (credenciado == null) {
+      throw new RuntimeException("O dados do credenciado sao de preenchimento obrigatorio");
+    }
 
-	public boolean credenciaisExistem(Credenciado credenciado){
-		return getDao().existsByEmailAndSenha(credenciado.getEmail(), credenciado.getSenha());
-	}
+    if (getDao().existsByEmail(credenciado.getEmail())) {
+      throw new RuntimeException("O usuário já existe");
+    }
+    
+  }
 
-	public Credenciado procurarPorEmail(String email){
-		return getDao().findByEmail(email);
-	}
+  public boolean emailExiste(String email) {
+    return getDao().existsByEmail(email);
+  }
+
+  public boolean existe(Long id) {
+    return getDao().existsById(id);
+  }
+
+  public boolean credenciaisExistem(Credenciado credenciado) {
+    return getDao().existsByEmailAndSenha(credenciado.getEmail(), credenciado.getSenha());
+  }
+
+  public Credenciado procurarPorEmail(String email) {
+    return getDao().findByEmail(email);
+  }
 
 }
--- src/main/java/br/upe/pweb/dodois/usuario/servico/interfaces/IUsuarioServico.java
+++ src/main/java/br/upe/pweb/dodois/usuario/servico/interfaces/IUsuarioServico.java
@@ -3,4 +3,7 @@ package br.upe.pweb.dodois.usuario.servico.interfaces;
 import br.upe.pweb.dodois.base.CrudService;
 import br.upe.pweb.dodois.usuario.model.Usuario;
 
-public interface IUsuarioServico extends CrudService<Usuario, Long> { }
+public interface IUsuarioServico extends CrudService<Usuario, Long> {
+  
+  Usuario incluir(Usuario usuario, Long idCredenciado);
+}
