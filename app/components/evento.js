0452c8a93ef64da3d0153ae6795345959b6ceac9
edgleyson <edgleyson.ferreira@upe.br>
2021-08-12 15:47:42 -0300

    :sparkles: Criado página e componente de evento.

---
+++ app/components/evento.js
@@ -0,0 +1,44 @@
+import { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+
+export default class Evento extends Component {
+
+    render() {
+        return (
+            <Container>
+                <Row xs="2" className="mt-3">
+                    <Col><Card>
+                        <CardBody>
+                            <CardTitle>Sintomas - 10</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Adicionar novo</a></CardSubtitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Todos</a></CardSubtitle>
+                        </CardBody>
+                    </Card>
+                    </Col>
+                    <Col><Card>
+                        <CardBody>
+                            <CardTitle>Lembretes - 10</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Adicionar novo</a></CardSubtitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Todos</a></CardSubtitle>
+                        </CardBody>
+                    </Card></Col>
+                    <Col className="mt-3"><Card>
+                        <CardBody>
+                            <CardTitle>Diagnósticos - 1</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Adicionar novo</a></CardSubtitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Todos</a></CardSubtitle>
+                        </CardBody>
+                    </Card></Col>
+                    <Col className="mt-3"><Card>
+                        <CardBody>
+                            <CardTitle>Medicações - 2</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Adicionar novo</a></CardSubtitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted"><a href="#">Todos</a></CardSubtitle>
+                        </CardBody>
+                    </Card></Col>
+                </Row>
+            </Container>
+        )
+    }
+}
