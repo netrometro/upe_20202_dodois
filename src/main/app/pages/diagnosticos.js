491426147af8545b77cd3ad5266c8b362292634a
Savio Santos <saviosa08@gmail.com>
2021-08-23 15:04:55 -0300

    :construction: Work in progress.

---
+++ src/main/app/pages/diagnosticos.js
@@ -0,0 +1,65 @@
+import { Component } from "react";
+import { Container } from "reactstrap";
+import { Card, CardBody, CardTitle } from "reactstrap";
+import { Table } from "reactstrap";
+import MenuUsuario from "../components/menuUsuario";
+import Link from "next/link";
+
+export default class Diagnosticos extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <MenuUsuario />
+                <Container>
+                    <Card className="mt-3">
+                        <CardBody>
+                            <CardTitle tag="h5" >Diagnósticos</CardTitle>
+                        </CardBody>
+                    </Card>
+                    <Card className="mt-3">
+                        <CardBody>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Parente</th>
+                                        <th>Data</th>
+                                        <th>Diagnóstico</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>1</th>
+                                        <td>Fulano</td>
+                                        <td>12/08/2021</td>
+                                        <td>Dermatite de contato devido...</td>
+                                        <td><Link href="#">ver mais</Link></td>
+                                    </tr>
+                                    <tr>
+                                        <th>2</th>
+                                        <td>Cicrano</td>
+                                        <td>12/08/2021</td>
+                                        <td>Dermatite de contato devido...</td>
+                                        <td><Link href="#">ver mais</Link></td>
+                                    </tr>
+                                    <tr>
+                                        <th>3</th>
+                                        <td>Beltranon</td>
+                                        <td>12/08/2021</td>
+                                        <td>Dermatite de contato devido...</td>
+                                        <td><Link href="#">ver mais</Link></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </CardBody>
+                    </Card>
+                </Container>
+            </div>
+        )
+    }
+}
---
+++ src/main/app/pages/lembretes.js
@@ -0,0 +1,65 @@
+import { Component } from "react";
+import { Container } from "reactstrap";
+import { Card, CardBody, CardTitle } from "reactstrap";
+import { Table } from "reactstrap";
+import MenuUsuario from "../components/menuUsuario";
+import Link from "next/link";
+
+export default class Lembretes extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <MenuUsuario />
+                <Container>
+                    <Card className="mt-3">
+                        <CardBody>
+                            <CardTitle tag="h5" >Lembretes</CardTitle>
+                        </CardBody>
+                    </Card>
+                    <Card className="mt-3">
+                        <CardBody>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Parente</th>
+                                        <th>Data</th>
+                                        <th>Descrição</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>1</th>
+                                        <td>Fulano</td>
+                                        <td>Amanhã 08h30</td>
+                                        <td>Comprar Dipirona, dipirona e dipirona...</td>
+                                        <td><Link href="#">ver mais</Link></td>
+                                    </tr>
+                                    <tr>
+                                        <th>2</th>
+                                        <td>Fulano</td>
+                                        <td>Amanhã 08h30</td>
+                                        <td>Comprar Dipirona, dipirona e dipirona...</td>
+                                        <td><Link href="#">ver mais</Link></td>
+                                    </tr>
+                                    <tr>
+                                        <th>3</th>
+                                        <td>Fulano</td>
+                                        <td>Amanhã 08h30</td>
+                                        <td>Comprar Dipirona, dipirona e dipirona...</td>
+                                        <td><Link href="#">ver mais</Link></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </CardBody>
+                    </Card>
+                </Container>
+            </div>
+        )
+    }
+}
