3f23ad449922a5792a102f01965f1a4bda84d17d
edgleyson <edgleyson.ferreira@upe.br>
2021-09-01 12:38:16 -0300

    :lipstick: Estilizado menu de usuario.

--- src/main/app/components/menuUsuario.js
+++ src/main/app/components/menuUsuario.js
@@ -6,26 +6,26 @@ export default class MenuUsuario extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-md navbar-light bg-light">
-                    <Link className="navbar-brand" href="/">Tracker de Dodois</Link>
+                <nav className="navbar navbar-expand-md">
+                <a href="/"><img src="https://i.imgur.com/Vugdwag.png" className="m-2 mt-2" height="40px" /></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse justify-content-end m-3" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/">Página Inicial</Link>
+                            <li className="nav-item m-1 me-md-4">
+                                <Link className="nav-link" href="/"><a className="text-decoration-none" style={{color:"#216651"}}>Página Inicial</a></Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#">Perfil</Link>
+                            <li className="nav-item m-1 me-md-4">
+                                <Link className="nav-link" href="#"><a className="text-decoration-none" style={{color:"#216651"}}>Perfil</a></Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#">Termos de Uso</Link>
+                            <li className="nav-item m-1 me-md-4">
+                                <Link className="nav-link" href="#"><a className="text-decoration-none" style={{color:"#216651"}}>Termos de Uso</a></Link>
                             </li>
                         </ul>
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#">Sobre</Link>
+                            <li className="nav-item m-1">
+                                <Link className="nav-link" href="#"><a className="text-decoration-none" style={{color:"#216651"}}>Sobre</a></Link>
                             </li>
                         </ul>
                     </div>
