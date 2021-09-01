e3a3232afbe8b8a528d32aeb566d4623203a23a8
brenolyES <brenoly.porto@upe.br>
2021-09-01 13:34:03 -0300

    :sparkles: criar pagina sobre

---
+++ src/main/app/pages/sobre.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { Container, Row, Col } from 'reactstrap';
+import Menu from "/components/menu";
+
+
+
+export default class sobre extends Component {
+    render() {
+        return (
+            <div>
+                <Menu/>
+                terminar
+            </div>
+        )
+    }
+}
