fad662b12b02172b0496f5f64e0ac5f25f9c5ca7
edgleyson <edgleyson.ferreira@upe.br>
2021-09-01 10:55:37 -0300

    :lipstick: Estilizado tela de login.

--- src/main/app/components/menu.js
+++ src/main/app/components/menu.js
@@ -8,7 +8,7 @@ export default class Menu extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-md navbar-light bg-light">
-                    <Image src="https://i.imgur.com/Vugdwag.png" className="m-2" height="35px"/>
+                    <img src="https://i.imgur.com/Vugdwag.png" className="m-2" height="35px"/>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/main/app/pages/login.js
+++ src/main/app/pages/login.js
@@ -4,20 +4,19 @@ import { Component } from "react";
 const style = {
     backgroundColor: "rgba(0, 0, 0, 0)",
     //boxShadow: "2px 2px 10px 0.1px black",
-    border: "1px solid black",
+    border: "1px solid #216651",
     borderRadius: "15px"
 }
-
 
 export default class Login extends Component {
 
     render() {
         return (
-            <div className="container-fluid d-flex justify-content-center">
+            <div className="container-fluid d-flex justify-content-center mt-5">
                 <div className="row col-sm-6 p-3 mt-5" style={style}>
                     <form>
                         <div className="mb-3" >
-                            <h3 className="d-flex justify-content-center">Entrar</h3>
+                            <h3 className="d-flex justify-content-center" style={{color:"#216651"}}>Entrar</h3>
                             <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email"/>
                         </div>
                         <div className="mb-3">
@@ -27,21 +26,19 @@ export default class Login extends Component {
                             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                             <label className="form-check-label" >Me Manter Conectado</label>
                         </div>
-                        <div className="d-flex justify-content-center" >
+                        <div className="d-flex justify-content-center " >
                             <div className="row col-sm-5">
-                                <button className="btn btn-secondary">Entrar</button>
+                                <button className="btn mt-5" style={{background:"#216651", color:"white"}}>Entrar</button>
                             </div>
                         </div>
-                        <div className="text-center">
-                            <Link href="url ">Ainda não sou cadastrado</Link>
+                        <div className="text-center text-decoration mt-3">
+                            <Link href="url "><a className="text-decoration-none" style={{color:"#216651"}}><b>Ainda não sou cadastrado</b></a></Link>
                         </div>
-                        <div className="text-center">
-                            <Link className="text-danger " href="url ">Esqueci minha senha</Link>
+                        <div className="text-center mt-3">
+                            <Link className="text-danger " href="url "><a className="text-decoration-none" style={{color:"#216651"}}><b>Esqueci minha senha</b></a></Link>
                         </div>
-
                     </form>
                 </div>
-
             </div>
         )
     }
